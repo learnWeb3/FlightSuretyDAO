eff8cbf44316873ae093b5605042814790e772a8
web3learn <antoinehadrienleguillou@outlook.com>
2021-08-14 22:24:57 +0200

    adding readline in order to ask user to initilaize the server with his wallet mnemonic

--- server/app.js
+++ server/app.js
@@ -1,90 +1,26 @@
-// FlightSuretyApp contract interface
-const FlightSuretyAppInterface = require("../client/src/contracts/FlightSuretyApp.json");
-// FlightSuretyOracle contract interface
-const FlightSuretyOracleInterface = require("../client/src/contracts/FlightSuretyOracle.json");
-// fetch current flights and helper to instantiate contract
-const { web3Contract, fetchFlights } = require("./src/index.js");
-// web3 libraries
-const Web3 = require("web3");
-// truffle hd wallet provider
-const HDWalletProvider = require("truffle-hdwallet-provider");
-// express
-const express = require("express");
-// node cron
-const cron = require("node-cron");
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 // dotenv
 const envPath = process.cwd() + "/../.env";
 require("dotenv").config({ path: envPath });
+// node cron
+const cron = require("node-cron");
 // environnent variables
-const { SERVER_PORT, MNEMONIC, PROVIDER_URL } = process.env;
-// websocket provider
-const wSSProviderURL = PROVIDER_URL.replace("ws", "http");
-// hdwalletprovider to access metamask wallet
-const httpProvider = new HDWalletProvider(MNEMONIC, PROVIDER_URL);
-// web3 instantiation
-const web3WSS = new Web3(wSSProviderURL);
-const web3HTTP = new Web3(httpProvider);
-// express instantiation
-const app = new express();
-
-cron.schedule("* * * * *", async () => {
-  try {
-    console.log("running cron tasks");
-    // fetch current network ID
-    const networkID = await web3WSS.eth.net.getId();
-    const appContractAddress =
-      FlightSuretyAppInterface.networks[networkID].address;
-    console.log(`FlightSuretyApp contract Address: ${appContractAddress}`);
-    // FlightSuretyApp contract instantiation
-    const appContract = web3Contract(
-      web3WSS,
-      appContractAddress,
-      FlightSuretyAppInterface.abi
-    );
-    const oracleContractAddress =
-      FlightSuretyOracleInterface.networks[networkID].address;
-    console.log(`FlightSuretyOracle contract address: ${appContractAddress}`);
-    // FlightSuretyOracle contract instantiation
-    const oracleContract = web3Contract(
-      web3WSS,
-      oracleContractAddress,
-      FlightSuretyOracleInterface.abi
-    );
-    // user address
-    // const userAddress = httpProvider.addresses[0];
-    const userAddress = await web3WSS.eth
-      .getAccounts()
-      .then((account) => account[2]);
-    console.log(`current user address: ${userAddress}`);
-    // fetch the flights
-    const flights = await fetchFlights(appContract, oracleContract);
-    //loop through it and create a request for data settlement
-    await Promise.all(
-      flights.map(
-        async ({
-          estimatedDeparture,
-          estimatedArrival,
-          flightID,
-          flightRef,
-          oracleRequestIsPresent,
-          oracleActivatedIndex,
-        }) => {
-          if (estimatedArrival * 1000 < Date.now() && !oracleRequestIsPresent) {
-            const tx = await oracleContract.methods
-              .createRequest(flightID, flightRef)
-              .send({ from: userAddress, gas: 500000 });
-            console.log(
-              `New settlement request for flight ${flightID} successfully created on oracle contract`
-            );
-            console.log(`Transaction hash: ${tx.transactionHash}`);
-          }
-        }
-      )
-    );
-  } catch (error) {
-    console.log(error);
+const { PROVIDER_URL } = process.env;
+// init cron tasks helper function
+const { initCronTasks } = require("./src/index.js");
+// ask user to provider wallet MNEMONIC and init cron task to create secure auditable request for settlement data to a randomly selected oracle provider
+rl.question("Please enter your ether wallet mnemonic: \n", (MNEMONIC) => {
+  if (
+    (MNEMONIC && MNEMONIC.match(/\w+/g)?.length === 12) ||
+    MNEMONIC.match(/\w+/g)?.length === 24
+  ) {
+    initCronTasks(MNEMONIC, PROVIDER_URL);
+  } else {
+    console.log("Invalid mnemonic aborting ...");
+    rl.close();
   }
 });
-app.listen(SERVER_PORT, () =>
-  console.log(`server running on port ${SERVER_PORT}`)
-);
--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,12 @@
+// FlightSuretyApp contract interface
+const FlightSuretyAppInterface = require("../../client/src/contracts/FlightSuretyApp.json");
+// FlightSuretyOracle contract interface
+const FlightSuretyOracleInterface = require("../../client/src/contracts/FlightSuretyOracle.json");
+// web3 libraries
+const Web3 = require("web3");
+// truffle hd wallet provider
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
 const web3Contract = (provider, address, abi) => {
   const contract = new provider.eth.Contract(abi, address);
   const { fromWei, toWei } = provider.utils;
@@ -24,7 +33,9 @@ const fetchFlights = async (appContract, oracleContract) => {
       return await Promise.all(
         flights.map(async (flight) => ({
           ...flight,
-          ...(await appContract.methods.getFlightSettlementData(flight.flightID).call()),
+          ...(await appContract.methods
+            .getFlightSettlementData(flight.flightID)
+            .call()),
         }))
       );
     }
@@ -45,4 +56,73 @@ const fetchFlights = async (appContract, oracleContract) => {
   });
 };
 
-module.exports = { web3Contract, fetchFlights };
+const initCronTasks = async (MNEMONIC, PROVIDER_URL) => {
+  try {
+    // hdwalletprovider to access metamask wallet
+    const httpProvider = new HDWalletProvider(MNEMONIC, PROVIDER_URL);
+    // websocket provider
+    const wSSProviderURL = PROVIDER_URL.replace("ws", "http");
+    // web3 instantiation
+    const web3WSS = new Web3(wSSProviderURL);
+    const web3HTTP = new Web3(httpProvider);
+    console.log("running cron tasks");
+    // fetch current network ID
+    const networkID = await web3WSS.eth.net.getId();
+    const appContractAddress =
+      FlightSuretyAppInterface.networks[networkID].address;
+    console.log(`FlightSuretyApp contract Address: ${appContractAddress}`);
+    // FlightSuretyApp contract instantiation
+    const appContract = web3Contract(
+      web3WSS,
+      appContractAddress,
+      FlightSuretyAppInterface.abi
+    );
+    const oracleContractAddress =
+      FlightSuretyOracleInterface.networks[networkID].address;
+    console.log(`FlightSuretyOracle contract address: ${appContractAddress}`);
+    // FlightSuretyOracle contract instantiation
+    const oracleContract = web3Contract(
+      web3WSS,
+      oracleContractAddress,
+      FlightSuretyOracleInterface.abi
+    );
+    // user address
+    // const userAddress = httpProvider.addresses[0];
+    const userAddress = await web3WSS.eth
+      .getAccounts()
+      .then((account) => account[2]);
+    console.log(`current user address: ${userAddress}`);
+    // fetch the flights
+    const flights = await fetchFlights(appContract, oracleContract);
+    //loop through it and create a request for data settlement
+    await Promise.all(
+      flights.map(
+        async ({
+          estimatedArrival,
+          flightID,
+          flightRef,
+          oracleRequestIsPresent,
+          insuredValue,
+        }) => {
+          if (
+            estimatedArrival * 1000 < Date.now() &&
+            !oracleRequestIsPresent &&
+            insuredValue > 0
+          ) {
+            const tx = await oracleContract.methods
+              .createRequest(flightID, flightRef)
+              .send({ from: userAddress, gas: 500000 });
+            console.log(
+              `New settlement request for flight ${flightID} successfully created on oracle contract`
+            );
+            console.log(`Transaction hash: ${tx.transactionHash}`);
+          }
+        }
+      )
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { initCronTasks };
